e2f2c50e63b0e9ea609120a55ccda0c4307328ba
Gareth Dobson <garethdobson@Jerrys-MacBook-Air.local>
2021-01-30 14:17:52 +0000

    Implementing POST and DELETE api routes

--- routes/api/items.js
+++ routes/api/items.js
@@ -13,4 +13,27 @@ router.get('/', (req, res) => {
         .then(items => res.json(items))
 });
 
+// @route POST api/items
+// @desc Create a post
+// @access Public
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name,
+        transport: req.body.transport,
+        distance: req.body.distance,
+        emissions: req.body.emissions,
+    });
+
+    newItem.save().then(item => res.json(item));
+});
+
+// @route DELETE api/items
+// @desc Delete a post
+// @access Public
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(error => res.status(404).json({ success: false }));
+});
+
 module.exports = router;
