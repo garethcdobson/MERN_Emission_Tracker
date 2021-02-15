d357bbca9f9af58dcac6f0587e461dd56835a416
Gareth Dobson <garethdobson@Jerrys-MacBook-Air.local>
2021-02-15 22:00:46 +0000

    Adding pagination to the emissions data table by implementing and adapting the reactstrap pagination component

--- client/src/components/EmissionTable.js
+++ client/src/components/EmissionTable.js
@@ -1,40 +1,83 @@
 import React, { Component } from 'react';
-import { Table } from 'reactstrap';
+import { Pagination, PaginationItem, PaginationLink, Table } from 'reactstrap';
 import { connect } from 'react-redux';
 import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
 import RemoveItemModal from './RemoveItemModal';
 
 class EmissionTable extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPage: 0,
+            pagesCount: 3,
+            pageSize: 3
+        };
+    };
 
     componentDidMount() {
         this.props.getItems();
-    }
+    };
 
     handleDeleteItem = (id) => {
         this.props.deleteItem(id);
-    }
+    };
+
+    setCurrentPage = (page) => {
+        this.setState({
+            currentPage: page
+        })
+    };
+
+    handlePageClick = (e, index) => {
+        e.preventDefault();
+        this.setCurrentPage(index);
+    };
+
+    handleFirstClick = (e) => {
+        e.preventDefault();
+        this.setCurrentPage(0);
+    };
+
+    handlePreviousClick = (e) => {
+        e.preventDefault();
+        this.setCurrentPage(this.state.currentPage - 1);
+    };
+
+    handleLastClick = (e) => {
+        e.preventDefault();
+        this.setCurrentPage(this.state.pagesCount);
+    };
+
+    handleNextClick = (e) => {
+        e.preventDefault();
+        this.setCurrentPage(this.state.currentPage + 1);
+    };
 
     render() {
         const { items } = this.props.item;
+        const { currentPage, pageSize, pagesCount} = this.state;
 
         return(
             <div className="emission-table">
                 <Table striped size="sm" responsive hover>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Description</th>
-                            <th>Transport</th>
-                            <th>Distance (km)</th>
-                            <th>CO2 Emissions (kgs)</th>
-                            <th>Date</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {items.map(({ _id, name, description, transport, distance, emissions, date }) => (
-                            <tr>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Transport</th>
+                        <th>Distance (km)</th>
+                        <th>CO2 Emissions (kgs)</th>
+                        <th>Date</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items
+                        .slice(currentPage * pageSize, (currentPage + 1) * pageSize)
+                        .map(({_id, name, description, transport, distance, emissions, date}) => {
+                        return (
+                            <tr key={_id}>
                                 <td>{name}</td>
                                 <td>{description}</td>
                                 <td>{transport}</td>
@@ -45,9 +88,31 @@ class EmissionTable extends Component {
                                     <RemoveItemModal onRemoveItem={this.handleDeleteItem.bind(this, _id)}/>
                                 </td>
                             </tr>
-                        ))}
+                        );
+                    })}
                     </tbody>
                 </Table>
+                <Pagination>
+                    <PaginationItem disabled={currentPage <= 0}>
+                        <PaginationLink onClick={this.handleFirstClick} first />
+                    </PaginationItem>
+                    <PaginationItem disabled={currentPage <= 0}>
+                        <PaginationLink onClick={this.handlePreviousClick} previous />
+                    </PaginationItem>
+                    {[...Array(pagesCount)].map((page, i) => (
+                        <PaginationItem active={i === currentPage} key={i}>
+                            <PaginationLink onClick={e => this.handlePageClick(e, i)}>
+                                {i + 1}
+                            </PaginationLink>
+                        </PaginationItem>
+                    ))}
+                    <PaginationItem disabled={(currentPage >= pagesCount - 1)}>
+                        <PaginationLink onClick={this.handleNextClick} next />
+                    </PaginationItem>
+                    <PaginationItem disabled={(currentPage >= pagesCount - 1)}>
+                        <PaginationLink onClick={this.handleLastClick} last />
+                    </PaginationItem>
+                </Pagination>
             </div>
         )
     };
